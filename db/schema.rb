7e2f764c1c14bf64f90a6cc63366569c41e35d1e
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2022-02-11 16:26:19 -0600

    add book resource and authorize each action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_204046) do
+ActiveRecord::Schema.define(version: 2022_02_11_222227) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_12_18_204046) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "users"
 end
